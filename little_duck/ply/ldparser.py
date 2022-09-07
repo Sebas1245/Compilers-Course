c7789dda53930c3cf34cb83b560c517fe3c3a3a2
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-09-07 17:03:07 -0500

    Adds fixes to parser and more test files

--- little_duck/ply/ldparser.py
+++ little_duck/ply/ldparser.py
@@ -13,24 +13,27 @@ def p_programa(p):
                 | PROGRAM ID ';' block'''
 
 def p_vars(p): 
-    '''vars : VAR identifier
-            | VAR identifier ':' type ';'  identifier'''
+    '''vars : VAR vars_p'''
+
+def p_vars_p(p):
+    '''vars_p : identifier ':' type ';' vars_p
+                | identifier ':' type ';' '''
 
 def p_identifier(p):
     '''identifier : ID ',' identifier 
-                  | ID
-                  | empty'''
+                  | ID'''
 
 def p_type(p):
     '''type : INT 
             | FLOAT'''
 
 def p_block(p):
-    '''block : '{' e '}' '''
+    '''block : '{' block_p '}' 
+            | '{' '}' '''
 
-def p_e(p):
-    '''e : statement e
-        | empty'''
+def p_block_p(p):
+    '''block_p : statement block_p
+        | statement'''
 
 def p_statement(p):
     '''statement : assignment
@@ -48,18 +51,12 @@ def p_expression(p):
 
 def p_exp(p):
     '''exp : term 
-            | exp_p'''
-
-def p_exp_p(p):
-    '''exp_p : term '+' term
+            | term '+' term
             | term '-' term'''
 
 def p_term(p):
     '''term : factor 
-            | term_p'''
-
-def p_term_p(p):
-    '''term_p : factor '*' factor
+            | factor '*' factor
             | factor '/' factor'''
 
 def p_factor(p):
@@ -74,31 +71,25 @@ def p_const_var(p):
                 | CTE_F'''
 
 def p_condition(p):
-    '''condition : IF '(' expression ')' block condition_p'''
-
-def p_condition_p(p):
-    '''condition_p : ';' 
-                   | ELSE block'''
+    '''condition : IF '(' expression ')' block ';' 
+                | IF '(' expression ')' block ELSE block ';' '''
 
 def p_write(p):
     '''write : PRINT '(' write_p ')' ';' '''
 
 def p_write_p(p):
-    '''write_p : expression ',' write_p
-                | CTE_STRING ',' write_p
-                | expression ','
-                | CTE_STRING ','
-                | expression
-                | CTE_STRING '''
+    '''write_p : write_p2
+                | write_p2 ',' write_p'''
+
+def p_write_p2(p):
+    '''write_p2 : expression
+                | CTE_STRING'''
 
 # Error rule for syntax errors
 def p_error(p):
     print("Syntax error in input!")
     raise Exception(p)
 
-def p_empty(p):
-    'empty :'
-    pass
 
 ld_parser = yacc.yacc(debug=True)
 
--- little_duck/ply/ply_test.py
+++ little_duck/ply/ply_test.py
@@ -1,8 +1,9 @@
 from ldparser import ld_parser
 
 def test():
-    print('Testing')
-    file = open('hello_world.ld')
+    print('Enter file name to be tested (with .ld extension)')
+    filename = input()
+    file = open(filename)
     input_str = file.read()
     file.close()
     ld_parser.parse(input_str)
